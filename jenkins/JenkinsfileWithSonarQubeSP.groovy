0e1bcd1f44ceaa081f19cf6870f117e80a2a6290
Kangs Passoubady <kpassoubady@gmail.com>
2021-08-26 21:31:28 -0700

    Added sonarqube

---
+++ jenkins/JenkinsfileWithSonarQubeSP.groovy
@@ -0,0 +1,21 @@
+node('win2') {
+    stage('SCM') {
+        def gitTool = tool 'GIT-WIN'
+        checkout scm
+    }
+
+    stage('SonarQube Analysis') {
+        def mvnHome = tool 'MVN-WIN';
+        def javaHome = tool 'JDK11-WIN'
+        withEnv([
+                "MVN_HOME=$mvnHome",
+                "JAVA_HOME=$javaHome",
+                "PATH=.:C:\\WINDOWS\\SYSTEM32;$mvnHome\\bin;$javaHome\\bin;$PATH"
+        ]) {
+            echo "PATH=$PATH"
+            withSonarQubeEnv() {
+                bat(/"%MVN_HOME%\bin\mvn" sonar:sonar/)
+            }
+        }
+    }
+}
